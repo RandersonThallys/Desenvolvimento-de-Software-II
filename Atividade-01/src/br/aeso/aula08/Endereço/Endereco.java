69702e7455d8fb1078eacf9231e0af3848dfe810
RandersonThallys <Randerson192@gmail.com>
2016-03-15 01:33:23 -0300

    atividade

---
+++ Atividade-01/src/br/aeso/aula08/Endereço/Endereco.java
@@ -0,0 +1,88 @@
+package br.aeso.aula08.Endereço;
+
+import br.aeso.aula08.Fornecedor.Fornecedor;
+
+public class Endereco {
+	
+	private String rua;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String cep;
+	private Fornecedor fornecedor;
+	
+	
+	public Endereco(){}
+	
+	public Endereco(String rua, String numero, String complemento, String bairro, String cidade, String cep, Fornecedor fornecedor){
+		
+		this.rua = rua;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.cep = cep;
+		this.fornecedor = fornecedor;
+		
+		
+		
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+
+}
